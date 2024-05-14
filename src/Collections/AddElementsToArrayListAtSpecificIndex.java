611e2709c45c286caf5771c8bed16ec659c247e3
Cami <calincamelia03@gmail.com>
2024-05-14 14:12:12 +0300

    update

---
+++ src/Collections/AddElementsToArrayListAtSpecificIndex.java
@@ -0,0 +1,18 @@
+package Collections;
+
+import java.util.ArrayList;
+
+/*ex. 2 Create a program to add elements to ArrayList at specific index.*/
+public class AddElementsToArrayListAtSpecificIndex {
+    public static void main(String[] args) {
+        ArrayList<String> movies = new ArrayList<>();
+        movies.add("Avatar");
+        movies.add("Catch if you can");
+
+        System.out.println(movies);
+
+        movies.add(0, "Batman");
+
+        System.out.println(movies);
+    }
+}
---
+++ src/Collections/CreateAndAddElementsToAnArrayList.java
@@ -0,0 +1,23 @@
+package Collections;
+
+import java.util.ArrayList;
+/* ex. 1 Create a program to create and add elements to an arraylist.*/
+import java.util.ArrayList;
+
+public class CreateAndAddElementsToAnArrayList {
+    public static void main(String[] args) {
+        // Create a new ArrayList to store strings
+        ArrayList<String> series = new ArrayList<>();
+
+        // Add elements to the ArrayList
+        series.add("Game of Thrones");
+        series.add("Breaking Bad");
+        series.add("Stranger Things");
+
+        // Print the ArrayList
+        System.out.println("TV Series: " + series);
+    }
+}
+
+
+
